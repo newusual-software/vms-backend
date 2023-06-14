df2fa42f561c8dce1f099129aaaa573af88a271c
dev-phantom <godsfavour1975@gmail.com>
2023-06-14 19:08:39 +0100

    fix: fixed all issues

--- models/user.js
+++ models/user.js
@@ -1,37 +1,46 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: [true, 'please add name'],
-        maxlength: 32       
-    },
-    email: {
-        type: String,
-        trim: true,
-        required: [true, 'please add email'],
-        unique: true,
-        match: [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            'Please enter a valid email address'
-        ]      
-    },
 
-    password: {
-        type: String,
-        trim: true,
-        required: [true, 'please add password'],
-        minlength: [6, 'password must have at least six(6) character'],
-        match: [
-            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{6,}$/,
-            'Password must contain at least one uppercase letter, one lowercase letter, one digit, one special character, and be at least six characters long'
-        ]
-    },
-    role: {
-        type:Number,
-        default:0,
-    }
-}, {timestamps: true})
+   name: {
+       type: String,
+       trim: true,
+       required : [true, 'Please add a Name'],
+       maxlength: 32
+   },
+
+   email: {
+       type: String,
+       trim: true,
+       required : [true, 'Please add a E-mail'],
+       unique: true,
+       match: [
+           /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+           'Please add a valid E-mail'
+       ]
+
+   },
+
+   password: {
+       type: String,
+       trim: true,
+       required : [true, 'Please add a Password'],
+       minlength: [6, 'password must have at least six(6) characters'],
+       match: [
+           /^(?=.*\d)(?=.*[@#\-_$%^&+=§!\?])(?=.*[a-z])(?=.*[A-Z])[0-9A-Za-z@#\-_$%^&+=§!\?]+$/,
+           'Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number and a special characters'
+       ]
+   },
+
+   role: {
+       type: Number,
+       default: 0,
+  
+   },
+
+
+
+}, {timestamps: true});
+
 
 module.exports = mongoose.model("user", userSchema)
