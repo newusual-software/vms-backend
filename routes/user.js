918bf642ff9bd68be039cb2b7907556a18a4e597
dev-phantom <godsfavour1975@gmail.com>
2023-06-15 01:14:04 +0100

    completed the user api

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require("../controllers/user")
-
+const { signup, signin, logout, singleUser } = require("../controllers/user")
 
 router.post('/signup', signup);
+router.post("/signin", signin);
+router.get("/logout", logout);
+router.get("/user/:id", singleUser);
 
 module.exports = router;
